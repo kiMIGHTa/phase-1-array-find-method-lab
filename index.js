4be90044265b384fccbf133674eb884ef527af4c
Dennis Kimaita <dkimaita22@gmail.com>
2023-06-20 09:20:54 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+const record = [
+ { year: "2015", result: "L"},
+ { year: "2014", result: "N/A"},
+ { year: "2013", result: "L"}
+]
+
+function superBowlWin(record){
+    
+     if(record.result==="W"){
+        return record.year
+     }else if(record!=="W"){
+      return undefined
+     }
+}
+record.find(superBowlWin)
